13ade9eaebddb96b5bc408b350afcd47ddf0bbea
Adam Burdett <burdettadam@gmail.com>
2018-05-26 13:11:53 -0600

    start discover service after 7 seconds.
    this should allow the pico engine to start up and be ready for any new discover events.

--- packages/pico-engine-core/src/modules/discover.js
+++ packages/pico-engine-core/src/modules/discover.js
@@ -3,16 +3,10 @@ var mkKRLfn = require("../mkKRLfn");
 var Discover = require("node-discover");
 var mkKRLaction = require("../mkKRLaction");
 
-var event = {eci   : root_eci,
-             eid   : "12345",
-             domain: "discover",
-            };
-
-module.exports = function(core){
-    var d = Discover({
-    //helloInterval: 1000, // How often to broadcast a hello packet in milliseconds
-    //checkInterval: 2000, // How often to to check for missing nodes in milliseconds
-    //nodeTimeout: 2000, // Consider a node dead if not seen in this many milliseconds
+var config = {
+    helloInterval: 3000, // How often to broadcast a hello packet in milliseconds
+    checkInterval: 6000, // How often to to check for missing nodes in milliseconds
+    nodeTimeout: 3000, // Consider a node dead if not seen in this many milliseconds
     //masterTimeout: 2000, // Consider a master node dead if not seen in this many milliseconds
     //mastersRequired: 1, // The count of master processes that should always be available
     //weight: Math.random(), // A number used to determine the preference for a specific process to become master. Higher numbers win.
@@ -28,51 +22,68 @@ module.exports = function(core){
 
     //ignore: 'self', // Which packets to ignore: 'self' means ignore packets from this instance, 'process' means ignore packets from this process
     //ignoreDataErrors: true // whether to ignore data errors including parse errors
-    });
+    };
 
-    core.db.listResources(function(err,resources){
-      d.advertise({
-        name: "PicoEngine",
-        resources: resources,
-        _host : core.host
+var event = {eci   : root_eci,
+             eid   : "12345",
+             domain: "discover",
+            };
+
+module.exports = function(core){
+
+    var d;
+    function startD(config,d){
+      
+      d = Discover(config);
+
+      core.db.listResources(function(err,resources){
+        d.advertise({
+          name: "PicoEngine",
+          resources: resources,
+          _host : core.host
+        });
       });
-    });
-    
+      
 
-    d.on('added', function(obj) {
-      console.log('A new node has been added.');
-      obj.discoverId = obj.id;
-      core.db.listObservers(function(err,observers){
-        for (var i = 0; i < observers.length; i++) {
-          event.eci = observers[i];
-          event.type = "engine_found";
-          event.attrs = obj;
-          pe.signalEvent(event, function(err, response){ /*if(err) return errResp(res, err); */});
+      d.on('added', function(obj) {
+        console.log('A new node has been added.');
+        obj.discoverId = obj.id;
+        core.db.listObservers(function(err,observers){
+          for (var i = 0; i < observers.length; i++) {
+            event.eci = observers[i];
+            event.type = "engine_found";
+            event.attrs = obj;
+            pe.signalEvent(event, function(err, response){ /*if(err) return errResp(res, err); */});
 
-         // request.post(
-         // "http://localhost:8080/sky/event/"+observers[i]+"/12345/discover/engine_found",
-         // { json: obj },
-         // function (error, response, body) { });
-        }
+           // request.post(
+           // "http://localhost:8080/sky/event/"+observers[i]+"/12345/discover/engine_found",
+           // { json: obj },
+           // function (error, response, body) { });
+          }
+        });
       });
-    });
 
-    d.on('removed', function(obj) {
-      console.log('A node has been removed.');
-      core.db.listObservers(function(err,observers){
-        for (var i = 0; i < observers.length; i++) { 
-          event.eci = observers[i];
-          event.type = "engine_lost";
-          event.attrs = obj;
-          pe.signalEvent(event, function(err, response){ /*if(err) return errResp(res, err); */});
+      d.on('removed', function(obj) {
+        console.log('A node has been removed.');
+        core.db.listObservers(function(err,observers){
+          for (var i = 0; i < observers.length; i++) { 
+            event.eci = observers[i];
+            event.type = "engine_lost";
+            event.attrs = obj;
+            pe.signalEvent(event, function(err, response){ /*if(err) return errResp(res, err); */});
 
-          //request.post(
-          //"http://localhost:8080/sky/event/"+observers[i]+"/12345/discover/engine_lost",
-          //{ json: obj  },
-          //function (error, response, body) { });
-        }
+            //request.post(
+            //"http://localhost:8080/sky/event/"+observers[i]+"/12345/discover/engine_lost",
+            //{ json: obj  },
+            //function (error, response, body) { });
+          }
+        });
       });
-    });
+    }
+
+    setTimeout(startD, 7000, config, d);// start discover service after engine starts
+
+
 
     return {
       def: {
